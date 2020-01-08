ea3e1ab0a510eec9c197abdaea8d2dbd0ee4cd00
Alex König <alex.koenig@outletcity.com>
2020-01-08 09:22:47 +0100

    Scoring Calculation

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="match">
-      
-      {{score}}
-      {{amountPlayer}}
-      {{checkout}}
+    {{ localScore }}
+    {{ amountPlayer }}
+    {{ checkout }}
+    <input v-model="shoot" v-on:keyup.enter="setNewScore()" />
   </div>
 </template>
 
 <script>
 export default {
-    props:[
-        'score',
-        'amountPlayer',
-        'checkout'
-    ]
-}
+  data() {
+    return {
+      shoot: "",
+      localScore: this.score
+    };
+  },
+  props: ["score", "amountPlayer", "checkout"],
+  methods: {
+    setNewScore() {
+      if (this.shoot > 60) alert("too high - max 60 possible");
+      let shoot = this.shoot;
+      let multiplicator = shoot.charAt(0);
+      if (multiplicator === "d") {
+        shoot = Number(shoot.slice(1)) * 2;
+      } else if (multiplicator === "t") {
+        shoot = Number(shoot.slice(1)) * 3;
+      }
+      this.localScore -= Number(shoot);
+      console.log(this.localScore);
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
