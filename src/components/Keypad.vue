a823174ce38ce2338a58fc1bfe8ed0449ae92a43
Alex König <alex.koenig@outletcity.com>
2020-01-29 16:44:46 +0100

    hamma

--- src/components/Keypad.vue
+++ src/components/Keypad.vue
@@ -1,5 +1,26 @@
 <template>
 	<div class="keypad">
+		<span @click="setMultiplikator('')">
+			<input
+				type="radio"
+				name="x01"
+			>
+			Single
+		</span>
+		<span @click="setMultiplikator('d')">
+			<input
+				type="radio"
+				name="x01"
+			>
+			Double
+		</span>
+		<span @click="setMultiplikator('t')">
+			<input
+				type="radio"
+				name="x01"
+			>
+			Triple
+		</span>
 		<span @click="addNum('1')">1</span>
 		<span @click="addNum('2')">2</span>
 		<span @click="addNum('3')">3</span>
@@ -17,51 +38,62 @@
 
 <script>
 export default {
-	name: 'Keypad',
+	name: "Keypad",
 	data() {
-		return{
-			input: ""
+		return {
+			input: "",
+			multiplicator: ""
 		};
 	},
-	methods:{
-		enter(){
-			this.$emit('KeypadEnter', this.input);
+	computed:{
+		multiValue(){
+			return this.multiplicator + this.input;
+		}
+	},
+	methods: {
+		enter() {
+			this.$emit("KeypadEnter", this.multiValue);
 			this.input = "";
+			this.multiplicator = "";
 		},
-		addNum(num){
+		addNum(num) {
 			this.input += num;
-			this.$emit('KeypadClicked', this.input);
+			this.$emit("KeypadClicked", this.multiValue);
 		},
-		removeLast(){
+		removeLast() {
 			this.input = this.input.slice(0, -1);
-			this.$emit('KeypadClicked', this.input);
+			this.$emit("KeypadClicked", this.multiValue);
+		},
+		setMultiplikator(multi){
+			this.multiplicator = multi;
+			this.$emit("KeypadClicked", this.multiValue);
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../styles/mixins.scss';
+@import "../styles/mixins.scss";
 
-.keypad{
-    height: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr;
-    border-bottom: 1px solid var(--divider-color);
-    border-left: 1px solid var(--divider-color);
-    @include desktop {
-        grid-column: 1 / span 2;
-    }
+.keypad {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  border-bottom: 1px solid var(--divider-color);
+  border-left: 1px solid var(--divider-color);
+  @include desktop {
+    grid-column: 1 / span 2;
+  }
 
-    span{
-        cursor: pointer;
-        min-height: 24px;
-        border-top: 1px solid var(--divider-color);
-        border-right: 1px solid var(--divider-color);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+  span {
+    cursor: pointer;
+    min-height: 24px;
+    border-top: 1px solid var(--divider-color);
+    border-right: 1px solid var(--divider-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
